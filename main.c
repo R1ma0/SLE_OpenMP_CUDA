024527ac80c91b12b9d700760d75cda0dd6b3af8
r1ma0 <cactus-2015@mail.ru>
2023-10-05 15:01:26 +0300

    Added time measurement function

--- main.c
+++ main.c
@@ -2,14 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
+#include <time.h>
+#include <string.h>
 
+typedef enum 
+{
+    SEC, // Seconds
+    MS,  // Milliseconds
+    MKS, // Microseconds
+} TimeFormat_t;
+
+double calcCPUTimeUsage(clock_t, clock_t, double);
+void displayExecutionTime(clock_t, clock_t, TimeFormat_t);
 void useGaussMethod(double **, double *, double *, unsigned int);
 void displayMatrix(double **, double *, unsigned int);
 void displaySolution(double *, unsigned int);
 
 int main()
 {
-    printf("Num threads: %d\n", omp_get_thread_num());
     unsigned int N = 3; // Number of equations
 
     // Memory allocation
@@ -24,29 +34,36 @@ int main()
 
     // Filling arrays
     
-    A[0][0] = 2.0;
-    A[0][1] = 4.0;
-    A[0][2] = 1.0;
-    A[1][0] = 5.0;
-    A[1][1] = 2.0;
-    A[1][2] = 1.0;
-    A[2][0] = 2.0;
-    A[2][1] = 3.0;
-    A[2][2] = 4.0;
+    A[0][0] = 2.351386;
+    A[0][1] = 4.721297;
+    A[0][2] = 1.983437;
+    A[1][0] = 5.534478;
+    A[1][1] = 2.375764;
+    A[1][2] = 1.392935;
+    A[2][0] = 2.894297;
+    A[2][1] = 3.154358;
+    A[2][2] = 4.653876;
 
-    Y[0] = 36.0;
-    Y[1] = 47.0;
-    Y[2] = 37.0;
+    Y[0] = 36.313643;
+    Y[1] = 47.645311;
+    Y[2] = 37.287654;
 
     // Calculate and display
 
     printf("Source matrix:\n");
     displayMatrix(A, Y, N);
 
+    clock_t execBegin = clock();
     useGaussMethod(A, Y, X, N);
+    clock_t execEnd = clock();
 
     printf("Solution:\n");
     displaySolution(X, N);
+
+    // Execution time
+   
+    TimeFormat_t format = MKS;
+    displayExecutionTime(execBegin, execEnd, format);
 
     // Freeing memory
 
@@ -173,4 +190,36 @@ void displaySolution(double *X, unsigned int N)
     {
         printf("X%d = %f\n", i + 1, X[i]);
     }
+}
+
+double calcCPUTimeUsage(clock_t begin, clock_t end, double divider)
+{
+    return ((double)(end - begin)) / (CLOCKS_PER_SEC / divider);
+}
+void displayExecutionTime(clock_t begin, clock_t end, TimeFormat_t format)
+{
+    double divider;
+    char title[4];
+
+    switch (format)
+    {
+        case SEC:
+            divider = 1.0;
+            strcpy(title, "sec");
+            break;
+        case MS:
+            divider = 1000.0;
+            strcpy(title, "ms");
+            break;
+        case MKS:
+            divider = 1000000.0;
+            strcpy(title, "mks");
+            break;
+        default:
+            divider = 1.0;
+            strcpy(title, "sec");
+    }
+
+    double time = calcCPUTimeUsage(begin, end, divider);
+    printf("Execution time: %f %s\n", time, title);
 }
