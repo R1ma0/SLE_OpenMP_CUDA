437d14d79975c447ed0c6afd5d7ba9c66defe405
r1ma0 <cactus-2015@mail.ru>
2023-10-07 15:10:37 +0300

    Cleaning

--- compile.sh
+++ compile.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-gcc main.c -o GaussMethod -Wall -pedantic -fopenmp
+
+echo "Compile 'main.c' ..."
+gcc main.c -o GaussMethod -Wall -pedantic 
+echo "Compile 'main_OpenMP.c' ..."
+gcc main_OpenMP.c -o GaussMethodOpenMP -Wall -pedantic -fopenmp
+echo "Compilation is complete!"
